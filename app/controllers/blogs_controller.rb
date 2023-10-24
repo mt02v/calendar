197cd812bc7a1349bebeab7b49b101d3feb3a11e
mt02v <141305346+mt02v@users.noreply.github.com>
2023-10-24 08:04:54 +0000

    [Finsh] calendar

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,40 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+    @blog = Blog.new
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def show
+    @blog = Blog.find(prams[:id])
+  end
+
+ def create
+   Blog.create(blog_parameter)
+   redirect_to blogs_path
+ end
+
+ def destroy
+   @blog = Blog.find(params[:id])
+   @blog.destory
+   redirect_to blogs_path, notice: "削除しました"
+ end
+
+ def edit
+   @blog = Blog.find(params[:id])
+   if @blog.update(blog_parameter)
+     redirect_to blog_path, notice: "編集しました"
+   else
+     render 'edit'
+   end
+ end
+
+private
+def blog_parameter
+  params.require(:blog).permit(:title, :content, :start_time)
+end
+
+end
